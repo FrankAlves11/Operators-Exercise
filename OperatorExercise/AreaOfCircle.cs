6fa639eb00be90780269f2f97409ad7fc7890ecd
FrankAlves11 <Frankie.alves@icloud.com>
2023-08-28 21:38:19 -0700

    made changes

---
+++ OperatorExercise/AreaOfCircle.cs
@@ -0,0 +1,33 @@
+﻿
+namespace AreaOfCircle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("17/4 is 4 remainder 1");
+
+            Console.WriteLine("What is the radius of your circle?");
+
+            
+            if (!double.TryParse(Console.ReadLine(), out var radius))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number for the radius.");
+                return;
+            }
+
+            
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
+
